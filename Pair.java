00b443fee907703cda6eadb9ed90d34537d17b96
therealdaddyj <31736482+therealdaddyj@users.noreply.github.com>
2021-09-19 12:43:56 -0300

    updating pair

--- Pair.java
+++ Pair.java
@@ -18,18 +18,6 @@ public class Pair {
         this.parent = parent;
     }
 
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Pair pair = (Pair) o;
-        return x == pair.x &&
-                y == pair.y;
-    }
- 
-    public int hashCode() {
-        return Objects.hash(x, y);
-    }
-    
     //Receives a character and returns a Number 
     public static int getNum(char x){
         int count = 0;
